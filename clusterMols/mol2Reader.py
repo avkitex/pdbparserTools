05db3a5777445ddee0d13da774bbef56719e4675
Avkitex <kit.iz.179@gmail.com>
2016-03-03 11:23:18 +0100

    Added comments

--- clusterMols/mol2Reader.py
+++ clusterMols/mol2Reader.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit import DataStructs
 
 def iterMol2(file):
     handle = open(file, 'r')
@@ -18,6 +20,31 @@ def iterMol2(file):
 
 def clusterMolecules(file):
     molecules=[]
+    simil = []
     for file in iterMol2(file):
         molecules.append(Chem.MolFromMol2Block(''.join(file)))
     print(len(molecules))
+    vects = [AllChem.GetMorganFingerprintAsBitVect(x,2,1024) for x in molecules]
+    for mol1 in range(len(vects)):
+
+        simil.append(DataStructs.BulkTanimotoSimilarity(vects[mol1], vects[:mol1]))
+    return simil
+    #dm=_DistanceMatrix(['mol' + str(x) for x in range(99)], mols[1:])
+    # tree = constructor.nj(dm)
+    #constructor = DistanceTreeConstructor()
+    #from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+    #from Bio.Phylo.TreeConstruction import _DistanceMatrix
+
+def ClusterFps(fps,cutoff=0.2):
+    from rdkit.ML.Cluster import Butina
+
+    # first generate the distance matrix:
+    dists = []
+    nfps = len(fps)
+    for i in range(1,nfps):
+        sims = DataStructs.BulkTanimotoSimilarity(fps[i],fps[:i])
+        dists.extend([1-x for x in sims])
+
+    # now cluster the data:
+    cs = Butina.ClusterData(dists,nfps,cutoff,isDistData=True)
+    return cs
