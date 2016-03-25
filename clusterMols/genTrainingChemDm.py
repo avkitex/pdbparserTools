defb4f0466955e8bdc7cb97bdc8c2f3fdf53524b
avkitex <kit.iz.179@gmail.com>
2016-03-25 10:19:56 +0000

    Deleted all not needed. Imported from clusterize.

--- clusterMols/genTrainingChemDm.py
+++ clusterMols/genTrainingChemDm.py
@@ -1,23 +1,10 @@
-from __future__ import print_function
-import os.path, argparse, gc
-from datetime import datetime
+import os.path, argparse, sys
 
-from rdkit import Chem
-from rdkit.Chem import AllChem
-from rdkit import DataStructs
-from rdkit.DataStructs.cDataStructs import SparseBitVect
-
-from Bio.Phylo.TreeConstruction import _DistanceMatrix
-from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
-from Bio import Phylo
-#from Bio.Phylo import draw
-
-from modules.positional.kitsite import *
-from modules.chem.mol2Reader import *
+from clusterize import getChemThainingCompondsAsVectors, genDistanceMatrixFileManyCompounds
+from clusterize import drawTree, distanceMatrixToTree
 
 parser = argparse.ArgumentParser(prog='genTrainingChemDm.py', usage='%(prog)s [options]', description='description',
 								 epilog="\xa9 Avktex 2016")
-parser.add_argument('-tf', '--trainingMol2', metavar='GlobConfig', type=str, help='Full path to multiMol2 training compounds docked file.', required=True)
 parser.add_argument('-dm', '--distanceMatrixOutput', metavar='GlobConfig', type=str, help='Full path to distance matrix output file.', required=True)
 #parser.add_argument('-inhl', '--inhibitorsList', metavar='GlobConfig', type=str, help='inhibitors list.', required=True)
 #parser.add_argument('-ninhl', '--notInhibitorsList', metavar='GlobConfig', type=str, help='Not inhibitors list.', required=True)
@@ -25,8 +12,6 @@ args = parser.parse_args()
 
 
 ############################ PARAMS #####################################
-if not os.path.isfile(args.trainingMol2):
-	print('Docked training compounds mol2 file is not exists')
 #if not os.path.isfile(args.inhibitorsList):
 #	print('Docked training compounds mol2 file is not exists')
 #if not os.path.isfile(args.notInhibitorsList):
@@ -42,59 +27,7 @@ notInhibitorsChsIds=[650, 682, 733, 864, 971, 1116, 1512, 1710, 2971, 3350, 5611
 392800, 4576521, 8257952, 20572534, 23122865, 23122927, 23123076, 112728, 10442445]
 
 
-def getFormatedTime():
-	return str(datetime.utcnow().strftime("[%H:%M:%S]"))
-
-def getSimilarityFromBitVectors(bitVectors):
-	simil = []
-	count = 0
-	n = 1
-	for mol1 in range(len(bitVectors)):
-		simil.append([1-x for x in DataStructs.BulkTanimotoSimilarity(bitVectors[mol1], bitVectors[:mol1+1])])
-		if count >= 100:
-			print(n * 100)
-			n += 1
-			count = 0
-		count += 1
-	return simil
-
-def getDistanceMatrix(names, bitVectors):
-	return _DistanceMatrix(names, getSimilarityFromBitVectors(bitVectors))
-
-def distanceMatrixToTree(distanceMatrix, method = False):
-	constructor = DistanceTreeConstructor()
-	if method:
-		return constructor.upgma(distanceMatrix)
-	else:
-		return constructor.nj(distanceMatrix)
-def drawTree(tree):
-	Phylo.draw_ascii(tree)
-	#Phylo.draw_graphviz(tree)
-	#pylab.show()
-def getChemBitVectorsArray(molecules):
-	return [AllChem.GetMorganFingerprintAsBitVect(x,2,1024) for x in molecules]
-def getChemThainingCompondsAsVectors():
-	print(getFormatedTime() + " Downloading ideal training molecules for chemical clustering")
-	chemNames, chemMolecules = getTrainingCompounds(inhibitorsChsIds, notInhibitorsChsIds, longNames = False)
-	print(getFormatedTime() + " Obtaining bit vectors representation")
-	return chemNames, getChemBitVectorsArray(chemMolecules)
-
-def genDistanceMatrixFileManyCompounds(ofile, names, vectors):
-	fh = open(ofile, 'w')
-	print('OutDistanceMatrix')
-	print(len(names), file=fh)
-
-	for cnum1 in range(len(names)):
-		if cnum1 % 100 == 0:
-			print(cnum1)
-		print (names[cnum1], file=fh, end = '')
-		bsimil = [1-x for x in DataStructs.BulkTanimotoSimilarity(vectors[cnum1], vectors[:cnum1])]
-		for sim in bsimil:
-			print('\t', "%.4f" % sim, sep='', end = '', file=fh)
-		print(file=fh)
-	fh.close()
-
 ############################ CHEM bitVectors #####################################
-namesC, vectorsC = getChemThainingCompondsAsVectors()
+names, vectors = getChemThainingCompondsAsVectors(inhibitorsChsIds, notInhibitorsChsIds, False)
 #drawTree(distanceMatrixToTree(getDistanceMatrix(namesC, vectorsC)))
-genDistanceMatrixFileManyCompounds(args.distanceMatrixOutput, namesC, vectorsC)
+genDistanceMatrixFileManyCompounds(args.distanceMatrixOutput, names, vectors)
