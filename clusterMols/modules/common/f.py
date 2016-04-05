76906373ee2a9229fdb1b1790a0fa2084252356b
avkitex <kit.iz.179@gmail.com>
2016-04-05 05:39:24 +0000

    Move log and error functions. Added json load and dump

--- clusterMols/modules/common/f.py
+++ clusterMols/modules/common/f.py
@@ -1,3 +1,15 @@
+from __future__ import print_function
+from datetime import datetime
+import json
+
+def getFormatedTime():
+	return str(datetime.utcnow().strftime("[%H:%M:%S]"))
+def logMsg(msg):
+	print(getFormatedTime(), msg)
+def errorMsg(msg):
+	print(getFormatedTime(), "ERROR:", msg)
+
+
 def iterMol2(file):
     handle = open(file, 'r')
     if handle:
@@ -11,3 +23,16 @@ def iterMol2(file):
         if len(curMol2):
             yield curMol2
         handle.close()
+
+def dumpJson(file, data):
+	with open(file, 'w') as out:
+		json.dump(data, out)
+	return 0
+def loadJson(file):
+	try:
+		with open(file, 'r') as inp:
+			data = json.load(inp)
+		return data
+	except Exception as e:
+		errorMsg("Not able to read json file. " + str(e))
+		return None
