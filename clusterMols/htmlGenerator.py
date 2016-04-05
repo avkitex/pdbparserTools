dfc6d2ed5d98b4c0ff318416f6fb2e5f12ae51eb
avkitex <kit.iz.179@gmail.com>
2016-04-05 05:41:01 +0000

    Slightly changed structure

--- clusterMols/htmlGenerator.py
+++ clusterMols/htmlGenerator.py
@@ -3,9 +3,9 @@ from Cheetah.Template import Template
 #{"ZINC12515747": { "energy":-11.6, "similar":331},"ZINC05288595": { "energy":-11.5, "similar":3321},"ZINC20580778": { "energy":-11.3, "similar":3361},"ZINC09190718": { "energy":-11.2, "similar":4331},"ZINC10157289": { "energy":-11.1, "similar":54731}}
 #"similarChem":[]
 #"similarContacts":[]
-def createHtmlReport(outFile, results, maxsimil=2, maxOutComp = 100):
+def createHtmlReport(outFile, results, maxsimil=2, maxOutComp = 100, iwidth=150, iheight=150):
 	templateDef=''.join(open('template.html').readlines())
-	nameSpace = {'maxSimilar':1, "results": results[:maxOutComp]}
+	nameSpace = {'maxSimilar':maxsimil, "results": results[:maxOutComp], "iwidth":iwidth, "iheight":iheight}
 	t = Template(templateDef, searchList=[nameSpace])
 	oHandle=open(outFile, 'w')
 	print(t, file=oHandle)
